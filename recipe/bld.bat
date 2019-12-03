6f33c9acb543feedfa3baaa7eb54ed9f98182da5
Dario Izzo <dario.izzo@gmail.com>
2019-12-03 14:21:39 +0100

    2.4.1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,19 +2,37 @@ mkdir build
 cd build
 
 cmake ^
-    -G "NMake Makefiles" ^
-    -DCMAKE_BUILD_TYPE=Release ^
+    -G "Ninja" ^
+    -DCMAKE_C_COMPILER=clang-cl ^
+    -DCMAKE_CXX_COMPILER=clang-cl ^
     -DBoost_NO_BOOST_CMAKE=ON ^
+    -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DBUILD_SPICE=yes ^
-    -DBUILD_MAIN=no ^
-    -DBUILD_PYKEP=yes ^
-    -DBUILD_TESTS=yes ^
+    -DPYKEP_BUILD_TESTS=no \
+    -DPYKEP_BUILD_SPICE=yes \
     ..
 
-cmake --build . --config Release
+cmake --build . -- -v
 
-ctest
+cmake --build . --target install
 
-cmake --build . --config Release --target install
+cd ..
+mkdir build_python
+cd build_python
+
+cmake ^
+    -G "Ninja" ^
+    -DCMAKE_C_COMPILER=clang-cl ^
+    -DCMAKE_CXX_COMPILER=clang-cl ^
+    -DBoost_NO_BOOST_CMAKE=ON ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DPYKEP_BUILD_PYKEP=yes \
+    -DPYKEP_BUILD_KEP_TOOLBOX=no \
+    ..
+
+cmake --build . -- -v
+
+cmake --build . --target install
